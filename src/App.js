78240b59eae88482d51299739226a4518991acd7
yuri <rhdbfl1770>
2020-12-27 21:57:42 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from "./components/Customer";
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+    root : {
+        width: '100%',
+        marginTop: theme.spacing.unit *3,
+        overflowX: "auto"
+    },
+    table: {
+        minWidth: 1080
+    }
+})
 
 const customers = [
     {
@@ -31,26 +49,40 @@ const customers = [
 
 class App extends Component {
     render() {
+        const { classes } = this.props;
         return(
-          <div>
-              {
-                  customers.map(c => {  //map을 사용할 때는 반드시 key값 정의
-                      return(
-                          <Customer
-                              key={c.id}
-                              id={c.id}
-                              image={c.image}
-                              name={c.name}
-                              age={c.age}
-                              gender={c.gender}
-                              live={c.live}
-                           />
-                      )
-                  })
-              }
-          </div>
+          <Paper className={classes.root}>
+              <Table className={classes.table}>
+                  <TableHead>
+                      <TableRow>
+                          <TableCell>아이디</TableCell>
+                          <TableCell>이미지</TableCell>
+                          <TableCell>이름</TableCell>
+                          <TableCell>나이</TableCell>
+                          <TableCell>성별</TableCell>
+                          <TableCell>주소지</TableCell>
+                      </TableRow>
+                  </TableHead>
+                  <TableBody>
+                      {customers.map(c => {  //map을 사용할 때는 반드시 key값 정의
+                              return(
+                                  <Customer
+                                      key={c.id}
+                                      id={c.id}
+                                      image={c.image}
+                                      name={c.name}
+                                      age={c.age}
+                                      gender={c.gender}
+                                      live={c.live}
+                                  />
+                              )
+                          })
+                      }
+                  </TableBody>
+              </Table>
+          </Paper>
         );
     }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,35 +1,18 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component {
     render() {
         return(
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo age={this.props.age} gender={this.props.gender} live={this.props.live}/>
-            </div>
-        )
-    }
-}
-
-class CustomerProfile extends React.Component {
-    render() {
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.age}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.live}</p>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.age}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.live}</TableCell>
+            </TableRow>
         )
     }
 }
